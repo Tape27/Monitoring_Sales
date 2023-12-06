7311f166754ea94098d94f1f93ccf8cfe8e7426c
Tape27 <avenmoney@gmail.com>
2023-12-06 15:45:47 +0300

    1

---
+++ FunPay_Monitoring_Bot/FunPay_Monitoring_Bot/Update_bases.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunPay_Monitoring_Bot
+{
+    internal class Update_bases
+    {
+        public static string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+        public static void Update_day(string name, int count, int day)
+        {
+            using (StreamWriter sw = File.AppendText(path + $"\\{name}\\{name}_day.txt"))
+            {
+                sw.WriteLine(day + "." + DateTime.Now.ToString("MM.yyyy") + " - " + count);
+            }
+        }
+    }
+}
